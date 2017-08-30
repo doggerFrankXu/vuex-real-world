f31f3954e84cc8c6b5c706c7dc2950ee2bef7d06
frank xu <frank.xu0203@gmail.com>
2017-08-30 23:59:44 +0800

    add user api call

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -1,22 +1,63 @@
 import User from '@/components/User.vue'
+
+const API_ROOT = 'https://api.github.com/'
+
+const callApi = (endPoint, schema) => {
+  const fullUrl = (endPoint.indexOf(API_ROOT) === -1) ? API_ROOT + endPoint : endPoint
+
+  return fetch(fullUrl)
+    .then(response =>
+      response.json().then(json => {
+        if (!response.ok) {
+          return Promise.reject(json)
+        }
+        return json
+      })
+    )
+}
+
+function loadUser (login) {
+  return callApi(`users/${login}`)
+}
+function loadStarred () {
+
+}
 export default {
   name: 'UserPage',
   components: {
     User
   },
-  props: [],
   data () {
     return {
-      user: {
-        login: 'yyx990803',
-        avatarUrl: 'https://avatars1.githubusercontent.com/u/499550?v=4',
-        name: 'Evan You'
-      }
+      user: null
     }
   },
   computed: {
     login () {
       return this.$route.params.login
     }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.loadData(vm)
+    })
+  },
+  beforeRouteUpdate (to, from, next) {
+    next()
+    this.loadData(this)
+  },
+  methods: {
+    loadData ({ login, loadUser, loadStarred }) {
+      // loadUser(login, [ 'name' ])
+      // loadStarred(login)
+
+      loadUser(login)
+        .then(response => {
+          this.user = response
+        })
+    },
+    loadUser,
+    loadStarred
   }
 }
+
