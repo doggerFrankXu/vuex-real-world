8ae772c049d45f4bea2f21d439c2bff727adcda2
frank xu <frank.xu0203@gmail.com>
2017-09-03 13:31:24 +0800

    update Repo component

--- src/components/Repo.vue
+++ src/components/Repo.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        {{owner}}
+    <div class="repo">
+        <h3>
+            <router-link :to="`/${login}/${name}`">
+                {{name}}
+            </router-link>
+            by
+            <router-link :to="`/${login}`">
+                {{login}}
+            </router-link>
+        </h3>
+        <p v-if="description">{{description}}</p>
     </div>
 </template>
 <script src="./Repo.js">
